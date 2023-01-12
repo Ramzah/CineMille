f8072111fc1ed01840f5d572c110905af71e2434
Ramzah <ramzah@hotmail.it>
2023-01-12 16:24:33 +0100

    Initial commit

---
+++ src/main/java/com/cinemille/gestione/CineMilleApplication.java
@@ -0,0 +1,13 @@
+package com.cinemille.gestione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineMilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinemille/gestione/CineMilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemille.gestione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineMilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
